15f3d11ebc001e8d58efbcdadeca269cdfb6a4a1
Deivid de Foggi <dfoggi@thefoggi.com>
2023-06-30 18:42:50 -0300

    Adjusted port to 8080.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Express server
 var express = require('express');
 var app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
